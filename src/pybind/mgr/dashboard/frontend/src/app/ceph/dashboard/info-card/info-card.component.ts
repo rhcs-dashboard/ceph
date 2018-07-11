cd8441cb408371ff69c79a37ce98d4931cfa613b
alfonsomthd <almartin@redhat.com>
2018-07-11 13:18:23 +0200

    mgr/dashboard: pass html content class by parameter in InfoCardComponent; set default view encapsulation. css cleanup
    Fixes: https://tracker.ceph.com/issues/24778
    
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/info-card/info-card.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/info-card/info-card.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cd-info-card',
   templateUrl: './info-card.component.html',
   styleUrls: ['./info-card.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class InfoCardComponent {
   @Input() title: string;
   @Input() link: string;
   @Input() cardClass = 'col-md-6';
   @Input() imageClass: string;
+  @Input() contentClass: string;
 }
