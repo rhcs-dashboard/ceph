10ae9cafb7280ed7ebadb81a06c76dff00dbf1fa
Ernesto Puerta <epuertat@redhat.com>
2019-11-28 19:11:42 +0100

    mgr/dashboard: fix Cephfs hang and tests
    This comes from https://github.com/ceph/ceph/pull/30691 commit
    60b530a7531b3ee263c12be174c0437c6347ac0b
    
    Fixes: https://tracker.ceph.com/issues/41372
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-chart/cephfs-chart.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-chart/cephfs-chart.component.spec.ts
@@ -9,6 +9,8 @@ describe('CephfsChartComponent', () => {
   let component: CephfsChartComponent;
   let fixture: ComponentFixture<CephfsChartComponent>;
 
+  const counter = [[0, 15], [5, 15], [10, 25], [15, 50]];
+
   configureTestBed({
     imports: [ChartsModule],
     declarations: [CephfsChartComponent]
@@ -17,6 +19,11 @@ describe('CephfsChartComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CephfsChartComponent);
     component = fixture.componentInstance;
+    component.mdsCounter = {
+      'mds_server.handle_client_request': counter,
+      'mds_mem.ino': counter,
+      name: 'a'
+    };
     fixture.detectChanges();
   });
 
