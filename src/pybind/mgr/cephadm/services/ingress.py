607812d928347e8a16df2dcf72d40269613db7b0
Redouane Kachach <rkachach@ibm.com>
2025-02-07 11:46:45 +0100

    mgr/cephadm: adding the SSL cert as a dependency for ingress service
    This way when the user changes the SSL certificate in the spec and
    performs an 'orch apply' cephadm will detect the change and
    reconfigure the ingress service automatically. In addition we are also
    adding the ssl_key because it was not included in the generated
    haproxy configuration.
    
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>
    (cherry picked from commit 8dbc5f84265035abe9f74aacbbeab2cc887359cc)
    
    Conflicts:
    	src/pybind/mgr/cephadm/services/ingress.py
    
    Resolves: rhbz#2344352

--- src/pybind/mgr/cephadm/services/ingress.py
+++ src/pybind/mgr/cephadm/services/ingress.py
@@ -97,10 +97,18 @@ class IngressService(CephService):
         # sufficient to detect changes.
         if not spec:
             return []
+
         ingress_spec = cast(IngressSpec, spec)
         assert ingress_spec.backend_service
         daemons = mgr.cache.get_daemons_by_service(ingress_spec.backend_service)
-        return sorted([d.name() for d in daemons])
+        deps = [d.name() for d in daemons]
+        for attr in ['ssl_cert', 'ssl_key']:
+            ssl_cert_key = getattr(ingress_spec, attr, None)
+            if ssl_cert_key:
+                assert isinstance(ssl_cert_key, str)
+                deps.append(f'ssl-cert-key:{str(utils.md5_hash(ssl_cert_key))}')
+
+        return sorted(deps)
 
     def haproxy_generate_config(
             self,
@@ -225,17 +233,17 @@ class IngressService(CephService):
                 "haproxy.cfg": haproxy_conf,
             }
         }
+
         if spec.ssl_cert:
-            ssl_cert = spec.ssl_cert
-            final_config['files']['haproxy.pem'] = ssl_cert
+            final_config['files']['haproxy.pem'] = spec.ssl_cert
+
         if spec.ssl_key:
-            ssl_key = spec.ssl_key
-            final_config['files']['haproxy.pem.key'] = ssl_key
+            final_config['files']['haproxy.pem.key'] = spec.ssl_key
 
         if spec.haproxy_qat_support:
             final_config['qat_support'] = True
 
-        return config_files, self.get_haproxy_dependencies(self.mgr, spec)
+        return final_config, self.get_haproxy_dependencies(self.mgr, spec)
 
     def keepalived_prepare_create(
             self,
