55374be59fd81f01ace4b7f9907d29d3283e1ef2
Ramana Raja <rraja@redhat.com>
2025-02-11 09:52:52 -0500

    librbd/io/AioCompletion: allow operations on group
    For asychronous group librbd APIs make use of existing
    librbd::io::AioCompletion class via the wrapper, RBD::AioCompletion.
    
    Get rid of RBD::AioGroupCompletion, and RBD::C_AioGroupCompletion that
    are no longer needed.
    
    Also modify the python RBD library to not make use of
    RBD::AioGroupCompletion.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>
    
    Resolves: rhbz#2089305

--- src/librbd/io/AioCompletion.h
+++ src/librbd/io/AioCompletion.h
@@ -22,6 +22,9 @@
 struct Context;
 
 namespace librbd {
+
+struct AsioEngine;
+
 namespace io {
 
 /**
@@ -63,6 +66,9 @@ struct AioCompletion {
   ImageCtx *ictx = nullptr;
   coarse_mono_time start_time;
   aio_type_t aio_type = AIO_TYPE_NONE;
+
+  librados::IoCtx group_ioctx;
+  AsioEngine* asio_engine = nullptr;
 
   ReadResult read_result;
 
@@ -110,8 +116,7 @@ struct AioCompletion {
   AioCompletion() {
   }
 
-  ~AioCompletion() {
-  }
+  ~AioCompletion();
 
   int wait_for_complete();
 
@@ -130,6 +135,7 @@ struct AioCompletion {
   void unblock(CephContext* cct);
 
   void init_time(ImageCtx *i, aio_type_t t);
+  void init_time(librados::IoCtx& group_ioctx_ref);
   void start_op();
   void fail(int r);
 
