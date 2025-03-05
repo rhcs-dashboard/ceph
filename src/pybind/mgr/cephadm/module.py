ed6320ba7da57a8b399c6d2c13ad9731b175117c
Redouane Kachach <rkachach@ibm.com>
2025-03-05 13:48:08 +0100

    mgr/cephadm: addressing reviewer comments
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>
    (cherry picked from commit ec2b3415b9514184c1e4a6454dea98bbe61da480)
    
    Resolves: rhbz#2351287

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -51,6 +51,7 @@ from mgr_module import (
     MgrModule,
     HandleCommandResult,
     Option,
+    OptionLevel,
     NotifyType,
     MonCommandFailed,
 )
@@ -409,6 +410,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
         ),
         Option(
             'certificate_check_debug_mode',
+            level=OptionLevel.DEV,
             type='bool',
             default=False,
             desc='FOR TESTING ONLY: This flag forces the certificate check instead of waiting for certificate_check_period.',
