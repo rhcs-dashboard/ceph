cf6f501ab24df30e5f0f6daecd933880499b74da
Aashish Sharma <Aashish.Sharma1@ibm.com>
2025-03-19 16:59:27 +0530

    mgr/dashboard: fix blockUI implementation
    Enable/Disable mgr modules from SMB page, Rgw multisite Page and Mgr Modules page doesn't give proper message and doesn't block the UI
    This PR intends to fix this issue
    
    Fixes: https://tracker.ceph.com/issues/70557
    
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/api/mgr-module.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/api/mgr-module.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { BlockUIService } from 'ng-block-ui';
 
 import { Observable, timer as observableTimer } from 'rxjs';
 import { NotificationService } from '../services/notification.service';
@@ -13,15 +13,13 @@ import { Router } from '@angular/router';
 export class MgrModuleService {
   private url = 'api/mgr/module';
 
-  @BlockUI()
-  blockUI: NgBlockUI;
-
   readonly REFRESH_INTERVAL = 2000;
 
   constructor(
     private http: HttpClient,
     private notificationService: NotificationService,
-    private router: Router
+    private router: Router,
+    private blockUI: BlockUIService
   ) {}
 
   /**
@@ -95,7 +93,7 @@ export class MgrModuleService {
             // Resume showing the notification toasties.
             this.notificationService.suspendToasties(false);
             // Unblock the whole UI.
-            this.blockUI.stop();
+            this.blockUI.stop('global');
             // Reload the data table content.
             if (table) {
               table.refreshBtn();
@@ -125,7 +123,7 @@ export class MgrModuleService {
         this.notificationService.suspendToasties(true);
         // Block the whole UI to prevent user interactions until
         // the connection to the backend is reestablished
-        this.blockUI.start($localize`Reconnecting, please wait ...`);
+        this.blockUI.start('global', $localize`Reconnecting, please wait ...`);
         fnWaitUntilReconnected();
       }
     );
