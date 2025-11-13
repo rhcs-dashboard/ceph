df1374c5763d9ef56d3e7ac44c1c89226a77c719
Raimund Sacherer <rsachere@redhat.com>
2025-11-13 20:56:36 +0100

    mgr: generate correct keyname for crash daemon
    Fixed the orchestrator to create the correct key name for crash daemons.
    The key was created with the hostname, while we add as authentication
    name `-n client.crash.<deamon-id>`.
    
    Fixes: https://tracker.ceph.com/issues/73847
    Signed-off-by: Raimund Sacherer <rsachere@redhat.com>
    (cherry picked from commit 52318847b5ef49b5ce1bacb6ed1d40f24d6db1f7)
    
    Conflicts:
    	src/pybind/mgr/cephadm/services/cephadmservice.py
    
    Resolves: rhbz#2432067
    
     Conflicts:
    	src/pybind/mgr/cephadm/services/cephadmservice.py

--- src/pybind/mgr/cephadm/services/cephadmservice.py
+++ src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -71,7 +71,7 @@ def get_auth_entity(daemon_type: str, daemon_id: str, host: str = "") -> AuthEnt
     # the CephService class refers to service types, not daemon types
     if daemon_type in ['rgw', 'rbd-mirror', 'cephfs-mirror', "iscsi", 'nvmeof', 'ingress', 'ceph-exporter', 'crash']:
         return AuthEntity(f'client.{daemon_type}.{daemon_id}')
-    elif daemon_type in ['crash', 'agent', 'node-proxy']:
+    elif daemon_type in ['agent', 'node-proxy']:
         if host == "":
             raise OrchestratorError(
                 f'Host not provided to generate <{daemon_type}> auth entity name')
